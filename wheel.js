5d20b2adf7c0b735d1b3214a828b2396c71c1609
darkdemon113 <nhkha15@gmail.com>
2016-11-30 03:27:34 +0700

    Update wheel.js

--- wheel.js
+++ wheel.js
@@ -1,11 +1,11 @@
-var canvasId="myDrawingCanvas";var wheelImageName=site+"/js/winwheel/prizewheel.png?v=4";var spinButtonImgOn=site+"/js/winwheel/spin_on.png?v=4";var spinButtonImgOff=site+"/js/winwheel/spin_off.png?v=4";var theSpeed=10;var pointerAngle=90;var doPrizeDetection=true;var spinMode="determinedAngle";var determinedGetUrl=site+"/game/vong-quay-may-man/?a=play";var prizes=new Array();prizes[0]={"name":"trúng 1000 bạc","startAngle":0,"endAngle":11};prizes[1]={"name":"ô trừ 500 bạc","startAngle":12,"endAngle":33};prizes[2]={"name":"danh hào ngẫu nhiên","startAngle":34,"endAngle":78};prizes[3]={"name":"nguyệt phiếu","startAngle":79,"endAngle":101};prizes[4]={"name":"ô may mắn lần sau","startAngle":102,"endAngle":168};prizes[5]={"name":"hoàng kim lệnh","startAngle":169,"endAngle":191};prizes[6]={"name":"trúng 200 bạc","startAngle":192,"endAngle":213};prizes[7]={"name":"danh hào ngẫu nhiên","startAngle":214,"endAngle":258};prizes[8]={"name":"hỏa tinh châu","startAngle":259,"endAngle":281};prizes[9]={"name":"ô may mắn lần sau","startAngle":282,"endAngle":348};prizes[10]={"name":"trúng 1000 bạc","startAngle":349,"endAngle":359};var surface;var wheel;var angle=0;var targetAngle=0;var currentAngle=0;var power=3;var xhr=new XMLHttpRequest();xhr.onreadystatechange=ajaxCallback;var randomLastThreshold=150;var spinTimer;var wheelState='reset';function begin()
+var canvasId="myDrawingCanvas";var wheelImageName=site+"/js/winwheel/prizewheel.png?v=4";var spinButtonImgOn=site+"/js/winwheel/spin_on.png?v=4";var spinButtonImgOff=site+"/js/winwheel/spin_off.png?v=4";var theSpeed=10;var pointerAngle=90;var doPrizeDetection=true;var spinMode="determinedAngle";var determinedGetUrl=site+"/game/vong-quay-may-man/?a=play";var prizes=new Array();prizes[0]={"name":"trúng 1000 bạc","startAngle":0,"endAngle":33};prizes[3]={"name":"nguyệt phiếu","startAngle":34,"endAngle":168};prizes[5]={"name":"hoàng kim lệnh","startAngle":169,"endAngle":191};prizes[6]={"name":"trúng 200 bạc","startAngle":192,"endAngle":213};prizes[8]={"name":"hỏa tinh châu","startAngle":214,"endAngle":281};prizes[10]={"name":"trúng 1000 bạc","startAngle":282,"endAngle":359};var surface;var wheel;var angle=0;var targetAngle=0;var currentAngle=0;var power=3;var xhr=new XMLHttpRequest();xhr.onreadystatechange=ajaxCallback;var randomLastThreshold=150;var spinTimer;var wheelState='reset';function begin()
 {surface=document.getElementById(canvasId);if(surface.getContext)
 {wheel=new Image();wheel.onload=initialDraw;wheel.src=wheelImageName;}}
 function initialDraw(e)
 {var surfaceContext=surface.getContext('2d');surfaceContext.drawImage(wheel,0,0);}
 function startSpin(determinedValue)
 {var stopAngle=undefined;if(spinMode=="random")
-{stopAngle=355;}
+{stopAngle=Math.floor(Math.random()*360);}
 else if(spinMode=="determinedAngle")
 {if(typeof(determinedValue)==='undefined')
 {if(determinedGetUrl)
@@ -13,15 +13,15 @@ else if(spinMode=="determinedAngle")
 else
 {if(determinedValue>360){alert("Để chơi trò này bạn cần 200 bạc.\nSố bạc trong tài khoản của bạn không đủ để chơi trò này");resetWheel();document.getElementById('spin_button').className="";wheelState='spinning';}
 else
-stopAngle=355;}}
+stopAngle=determinedValue;}}
 else if(spinMode=="determinedPrize")
 {if(typeof(determinedValue)==='undefined')
 {if(determinedGetUrl)
 {xhr.open('GET',determinedGetUrl,true);xhr.send('');}}
 else
-{stopAngle=354}
+{stopAngle=Math.floor(prizes[determinedValue]['startAngle']+(Math.random()*(prizes[determinedValue]['endAngle']- prizes[determinedValue]['startAngle'])));}}
 if((typeof(stopAngle)!=='undefined')&&(wheelState=='reset')&&(power))
-{stopAngle=355;targetAngle=(360*(power*6)+ stopAngle);randomLastThreshold=Math.floor(90+(Math.random()*90));document.getElementById('spin_button').className="";$(".power_controls").hide();wheelState='spinning';doSpin();}}
+{stopAngle=(360+ pointerAngle)- stopAngle;targetAngle=(360*(power*6)+ stopAngle);randomLastThreshold=Math.floor(90+(Math.random()*90));document.getElementById('spin_button').className="";$(".power_controls").hide();wheelState='spinning';doSpin();}}
 function ajaxCallback()
 {if(xhr.readyState<4)
 {return;}
